7d55e0c9f65700f51df1ed59574f5fec5fd20f96
Lasana Murray <lmurray@quenk.com>
2023-02-14 15:27:12 -0400

    build: remove lib dir

--- lib/app/auth/index.d.ts
+++
@@ -1,4 +0,0 @@
-/**
- * This module provides a framework for building a back-end workflow to simple
- * username and password based authentication forms.
- */
--- lib/app/auth/password.d.ts
+++
@@ -1,5 +0,0 @@
-import { Future } from '@quenk/noni/lib/control/monad/future';
-/**
- * compare two bcrypt hashed passwords for equality.
- */
-export declare const compare: (pwd1: string, pwd2: string) => Future<boolean>;
