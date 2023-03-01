f0b6e0f3cc1712339bf46b131b3f209c7390f642
Lasana Murray <lmurray@quenk.com>
2023-03-01 12:43:52 -0400

    close #6: add mongodb testkit

---
+++ src/app/db/mongodb/testkit.ts
@@ -0,0 +1,180 @@
+import * as mongo from 'mongodb';
+import * as noniClient from '@quenk/noni-mongodb/lib/client';
+import * as noniDb from '@quenk/noni-mongodb/lib/database';
+import * as noniCollection from '@quenk/noni-mongodb/lib/database/collection';
+
+import { merge } from '@quenk/noni/lib/data/record';
+import {
+    pure,
+    doFuture,
+    sequential,
+    Future
+} from '@quenk/noni/lib/control/monad/future';
+import { Type } from '@quenk/noni/lib/data/type';
+
+const defaultConfig = {
+
+    url: 'mongodb://localhost/testkittest',
+
+    collectionNames: [],
+
+    removeAllCollections: false,
+
+    dropDatabase: false
+
+};
+
+/**
+ * Configuration for Testkit instances.
+ */
+export interface Configuration {
+
+    /**
+     * url to use to connect the client.
+     *
+     * The environment variable MONGO_URL is used if this is not specified.
+     */
+    url: string,
+
+    /**
+     * collectionNames is an array of collection names that can be configured to 
+     * automically remove after each test.
+     */
+    collectionNames: string[],
+
+    /**
+     * removeAlLCollections if true, removes all collections after each test
+     * instead of the ones in collectionNames.
+     */
+    removeAllCollections: boolean,
+
+    /**
+     * dropDatabase if true, will drop the database at the end of testing.
+     */
+    dropDatabase: boolean
+
+}
+
+/**
+ * Testkit provides an API for manipulating a MongoDB database during testing.
+ */
+export class Testkit {
+
+    constructor(public __config: Partial<Configuration> = {}) { }
+
+    config: Configuration = merge(defaultConfig, this.__config);
+
+    client: mongo.MongoClient = <Type>undefined;
+
+    db: mongo.Db = <Type>undefined;
+
+    /**
+     * setUp initializes the client and connects to the database.
+     */
+    setUp = (): Future<void> => {
+
+        let that = this;
+
+        return doFuture(function*() {
+
+            let { url } = that.config;
+
+            that.client = yield noniClient.connect(url ?
+                url : <string>process.env.MONGO_URL);
+
+            that.db = that.client.db();
+
+            return pure(<void>undefined);
+
+        });
+
+    }
+
+    /**
+     * tearDown should be ran after each test so that the desired collections can
+     * be removed after each test.
+     */
+    tearDown = () => {
+
+        let that = this;
+
+        return doFuture<void>(function*() {
+
+            let { db, config } = that;
+            let { removeAllCollections, collectionNames } = config;
+
+            let names = (removeAllCollections === true) ?
+                (yield noniDb.collections(db))
+                    .map((c: mongo.Collection) => c.collectionName) :
+                collectionNames;
+
+            yield sequential(names.map((c: string) =>
+                noniDb.dropCollection(that.db, c)));
+
+            return pure(<void>undefined);
+
+        });
+
+    }
+
+    /*
+     * setDown should be ran at the end of the entire suite to drop the database
+     * and terminate the connection.
+     */
+    setDown = () => {
+
+        let that = this;
+
+        return doFuture<void>(function*() {
+
+            if (that.config.dropDatabase)
+                yield noniDb.drop(that.db);
+
+            let client = that.client;
+
+            that.client = <Type>undefined;
+            that.db = <Type>undefined;
+
+            return noniClient.disconnect(client);
+
+        });
+
+    }
+
+    /**
+     * removeCollection by name.
+     */
+    removeCollection =
+        (name: string) => noniDb.dropCollection(this.db, name);
+
+    /**
+     * populate a collection with the provided data.
+     */
+    populate = (collection: string, data: object[]) =>
+        noniCollection.insertMany(this.db.collection(collection), data);
+
+    /**
+     * find documents in a collection that match the provided query object.
+     */
+    find = (collection: string, qry: object, opts?:object) =>
+        noniCollection.find(this.db.collection(collection), qry, opts);
+
+    /**
+     * findOne document in a collection that matches the provided query object.
+     */
+    findOne = (collection: string, qry: object, opts?: object) =>
+        noniCollection.findOne(this.db.collection(collection), qry, opts);
+
+    /**
+     * update documents in a collection that match the provided query object.
+     */
+    update = (collection: string, qry: object, spec: object) =>
+        noniCollection.updateMany(this.db.collection(collection), qry, spec);
+
+    /**
+     * count the number of documetns occuring in a collection.
+     */
+    count = (collection: string, qry: object) =>
+        noniCollection.count(this.db.collection(collection), qry);
+
+}
---
+++ test/app/db/mongodb/testkit_test.ts
@@ -0,0 +1,219 @@
+import * as noniCollection from '@quenk/noni-mongodb/lib/database/collection';
+import * as noniDb from '@quenk/noni-mongodb/lib/database';
+
+import {
+    toPromise,
+    attempt,
+    doFuture,
+    liftP
+} from '@quenk/noni/lib/control/monad/future';
+import { assert } from '@quenk/test/lib/assert';
+
+import { Testkit } from '../../../../lib/app/db/mongodb/testkit';
+
+let kit = new Testkit({ dropDatabase: true, removeAllCollections: true });
+
+describe('Testkit', () => {
+
+    before(() => toPromise(kit.setUp()));
+
+    describe('setUp', () => {
+
+        it('should connect using MONGO_URL if no url specified', () =>
+            toPromise(doFuture(function*() {
+
+                process.env.MONGO_URL = 'mongodb://localhost/fooman';
+
+                let lkit = new Testkit({ url: <any>null });
+
+                yield lkit.setUp();
+
+                yield attempt(() =>
+                    assert(lkit.db.databaseName).equal('fooman'));
+
+                return liftP(() => lkit.client.close());
+
+            })));
+
+    });
+
+    describe('tearDown', () => {
+
+        it('should only remove intended collections', () =>
+            toPromise(doFuture(function*() {
+
+                let lkit = new Testkit({ collectionNames: ['pains'] });
+
+                yield lkit.setUp();
+                yield lkit.populate('names', [{ f: 1 }]);
+                yield lkit.populate('games', [{ f: 1 }]);
+                yield lkit.populate('pains', [{ f: 1 }]);
+                yield lkit.tearDown();
+
+                let res = yield noniDb.collections(lkit.db);
+
+                yield attempt(() => assert(res.length).equal(2));
+
+                return liftP(() => lkit.client.close());
+
+            })));
+
+        it('should remove all collections when told to', () =>
+            toPromise(doFuture(function*() {
+
+                let lkit = new Testkit({ removeAllCollections: true });
+
+                yield lkit.setUp();
+                yield lkit.populate('names', [{ f: 1 }]);
+                yield lkit.populate('games', [{ f: 1 }]);
+                yield lkit.populate('pains', [{ f: 1 }]);
+                yield lkit.tearDown();
+
+                let res = yield noniDb.collections(lkit.db);
+
+                yield attempt(() => assert(res.length).equal(0));
+
+                return liftP(() => lkit.client.close());
+
+            })));
+
+    });
+
+    describe('setDown', () => {
+
+        it('should not drop the database if not told to', () => {
+
+                  it('should remove all collections when told to', () =>
+            toPromise(doFuture(function*() {
+
+                let lkit = new Testkit();
+
+                yield lkit.setUp();
+                yield lkit.populate('names', [{ f: 1 }]);
+                yield lkit.populate('games', [{ f: 1 }]);
+                yield lkit.populate('pains', [{ f: 1 }]);
+                yield lkit.setDown();
+
+                let res = yield noniDb.collections(kit.db);
+
+                return attempt(() => assert(res.length).equal(3));
+
+            })));
+        });
+      
+    });
+
+    describe('populate', () => {
+
+        it('should populate data', () => toPromise(doFuture(function*() {
+
+            let data = [{ id: 1 }, { id: 2 }, { id: 3 }];
+
+            yield kit.populate('datums', data);
+
+            let results =
+                yield noniCollection.find(kit.db.collection('datums'), {});
+
+            return attempt(() => assert(results.length).equal(3));
+
+        })));
+
+    });
+
+    describe('removeCollection', () => {
+
+        it('should remove collections', () => toPromise(doFuture(function*() {
+
+            yield kit.populate('datums', [{ name: 1 }]);
+
+            let results =
+                yield noniCollection.find(kit.db.collection('datums'), {});
+
+            yield attempt(() => assert(results.length).equal(1));
+
+            yield kit.removeCollection('datums');
+
+            let newResults =
+                yield noniCollection.find(kit.db.collection('datums'), {});
+
+            return attempt(() => assert(newResults.length).equal(0));
+
+        })));
+
+    });
+
+    describe('find', () => {
+
+        it('should find documents', () => toPromise(doFuture(function*() {
+
+            yield kit.populate('datums',
+                [{ name: 1 }, { name: 2 }, { name: 1 }, { name: 1 }]);
+
+            let results =
+                yield kit.find('datums', { name: 1 });
+
+            return attempt(() => assert(results.length).equal(3));
+
+        })));
+
+    });
+
+    describe('count', () => {
+
+        it('should count documents', () => toPromise(doFuture(function*() {
+
+            yield kit.populate('datums',
+                [{ name: 1 }, { name: 2 }, { name: 1 }, { name: 1 }]);
+
+            let n =
+                yield kit.count('datums', { name: 1 });
+
+            return attempt(() => assert(n).equal(3));
+
+        })));
+
+    });
+
+    describe('findOne', () => {
+
+        it('should find a document', () => toPromise(doFuture(function*() {
+
+            yield kit.populate('datums',
+                [{ name: 1 }, { name: 2 }, { name: 1 }]);
+
+            let mresult =
+                yield kit.findOne('datums', { name: 2 });
+
+            return attempt(() => assert(mresult.isJust()).true());
+
+        })));
+
+    });
+
+    describe('update', () => {
+
+        it('should update documents', () => toPromise(doFuture(function*() {
+
+            yield kit.populate('datums',
+                [{ name: 1 }, { name: 2 }, { name: 1 }]);
+
+            let r =
+                yield kit.update('datums', { name: 1 }, { $set: { name: 2 } });
+
+            yield attempt(() => assert(r.modifiedCount).equal(2));
+
+            let results =
+                yield kit.find('datums', {});
+
+            return attempt(() => assert(results.length).equal(3));
+
+        })));
+
+    });
+
+    afterEach(() => toPromise(kit.tearDown()));
+
+    after(() => toPromise(kit.setDown()));
+
+});
+
