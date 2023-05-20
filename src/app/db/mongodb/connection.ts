299227db5c8dc6df5484e0b35634b005a9c88368
Lasana Murray <lmurray@quenk.com>
2023-05-20 13:48:31 -0400

    close #86: add create() to MongoDBConnection

--- src/app/db/mongodb/connection.ts
+++ src/app/db/mongodb/connection.ts
@@ -12,6 +12,19 @@ import { Future, pure, liftP } from '@quenk/noni/lib/control/monad/future';
 export class MongoDBConnection implements conn.Connection {
     constructor(public client: MongoClient) {}
 
+    /**
+     * create a new MongoDBConnection instance.
+     *
+     * Note that the actual connection to the database/cluster is only made when
+     * the open() method is called.
+     */
+    static create(
+        url: string,
+        opts: MongoClientOptions = {}
+    ): MongoDBConnection {
+        return new MongoDBConnection(new MongoClient(url, opts));
+    }
+
     open(): Future<void> {
         return liftP(() => this.client.connect()).map(() => {});
     }
@@ -28,7 +41,4 @@ export class MongoDBConnection implements conn.Connection {
 /**
  * connector for creating Connections to a MongoDB instance.
  */
-export const connector = (
-    url: string,
-    opts: MongoClientOptions = {}
-): MongoDBConnection => new MongoDBConnection(new MongoClient(url, opts));
+export const connector = MongoDBConnection.create;
